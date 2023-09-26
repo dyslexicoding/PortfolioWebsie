6c9d7e0b380d4fdb4857d79acf191f7eeaf0250a
dyslexicoding <sami@taylorcrew.us>
2023-09-27 01:28:47 +0300

    finishing at 1 am no biggy ...

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -11,8 +11,8 @@ const Intro = () => {
         <div className="IntroContent">
             <span className="Hello">Hello</span>
             <span className="IntroText">I'm  <span className="IntroName">Samuel</span> <br />Software Engineer </span>
-            <p className="IntroParagraph">Im and Skilled Engineer quick witted, loves puzzles and telling story with data.</p>
-            <Link><button className="btn"><img src={BtnImg} alt="HireMe" />Hire Me</button></Link>
+            <p className="IntroParagraph">Im and Skilled Engineer quick witted, loves puzzles <br /> and telling story with data.</p>
+            <Link><button className="btn"><img src={BtnImg} alt="HireMe" className='BtnImg' />Hire Me</button></Link>
         </div>
         <img src={bg} alt="profile" className='bg' />
 
